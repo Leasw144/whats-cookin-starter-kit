26e563829b37faf01aaf0ebd8b61db5b47301dd0
Sean Leas <phoenix_24leas.com>
2020-07-08 13:53:36 -0600

    Create tests for id property

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -8,7 +8,19 @@ describe('Ingredient', () => {
   }); 
 
   it('should be an instance of Ingredient', () => {
-    const ingredient = new Ingredient()
-    expect(ingredient).to.be.an.instanceof(Ingredient)
+    const ingredient = new Ingredient();
+    expect(ingredient).to.be.an.instanceof(Ingredient);
+  });
+
+  it('should have an ID', () => {
+    const ingredient = new Ingredient();
+    expect(ingredient.id).to.equal(1)
   })
+
+  it('should be able to take any number as an ID', () => {
+    const ingredient = new Ingredient(24);
+    expect(ingredient.id).to.equal(24)
+
+  })
+  
 });
