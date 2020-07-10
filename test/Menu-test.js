d8fe7e52ae4424207c1283678f489badbf21abdc
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-07-09 21:07:15 -0600

    Write tests for adding to menu

--- test/Menu-test.js
+++ test/Menu-test.js
@@ -2,8 +2,10 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Pantry = require('../src/Pantry');
+const Recipe = require('../src/Recipe');
 const Menu = require('../src/Menu');
-const recipeData = require('../data/recipes')
+const recipeData = require('../data/recipes');
+const users = require('../data/users');
 
 describe('Menu', () => {
 
@@ -23,17 +25,37 @@ describe('Menu', () => {
     expect(menu.recipesToCook).to.deep.equal([]);
   });
 
-  it('should allow the user to add a recipe to that menu', () => {
-    const recipe = recipeData[0];
+  it('should allow the user to add a recipe to the menu', () => {
+    const recipe = new Recipe(recipeData[0].id, recipeData[0].image, recipeData[0].ingredients, recipeData[0].instructions, recipeData[0].name, recipeData[0].tags);
+    const menu = new Menu();
+    
+    menu.addToMenu(recipe);
+    
+    expect(menu.recipesToCook[0].name).to.equal('Loaded Chocolate Chip Pudding Cookie Cups');
+  });
+  
+  it('should not allow the user to add duplicates of the same recipe to the menu', () => {
+    const recipe = new Recipe(recipeData[0].id, recipeData[0].image, recipeData[0].ingredients, recipeData[0].instructions, recipeData[0].name, recipeData[0].tags);
+    const recipe2 = new Recipe(recipeData[1].id, recipeData[1].image, recipeData[1].ingredients, recipeData[1].instructions, recipeData[1].name, recipeData[1].tags);
+    const menu = new Menu();
+    
+    menu.addToMenu(recipe);
+    menu.addToMenu(recipe);
+
+    expect(menu.recipesToCook.length).to.equal(1);
+    
+    menu.addToMenu(recipe2);
+
+    expect(menu.recipesToCook.length).to.equal(2);
+  });
+
+  it('should have an array of objects as its list of recipes to cook', () => {
+    const recipe = new Recipe(recipeData[0].id, recipeData[0].image, recipeData[0].ingredients, recipeData[0].instructions, recipeData[0].name, recipeData[0].tags);
     const menu = new Menu();
     
     menu.addToMenu(recipe);
 
-    expect(menu.recipesToCook[0].name).to.equal('Loaded Chocolate Chip Pudding Cookie Cups');
-  });
-
-  it('should have an array of objects as its list of recipes to cook', () => {
-
+    expect(menu.recipesToCook[0]).to.be.an('object');
   });
 
   it('should be able to give us a grocery list of ingredients still needed if the pantry does not have enough to cover the recipes on the menu', () => {
