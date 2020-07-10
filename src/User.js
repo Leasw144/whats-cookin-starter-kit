2b7c161656575de22bd0e59600921a6ce164873e
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-07-10 00:26:40 -0600

    Refactor toggle favorite

--- src/User.js
+++ src/User.js
@@ -2,21 +2,22 @@ const Menu = require("./Menu");
 
 class User {
   constructor(name, id, pantry) {
-    this.name = name || 'Guest';
+    this.name = name || "Guest";
     this.id = id || 0;
     this.pantry = pantry || [];
     this.favoriteRecipes = [];
     this.menu = new Menu();
   }
 
-  toggleFavorite(recipe) {
-    if (this.favoriteRecipes.includes(recipe)) {
-      let i = this.favoriteRecipes.indexOf(recipe);
-      this.favoriteRecipes.splice(i, 1);
+  toggleFavorite = (recipe) => {
+    const favs = this.favoriteRecipes;
+    if (favs.includes(recipe)) {
+      const i = favs.indexOf(recipe);
+      favs.splice(i, 1);
     } else {
-      this.favoriteRecipes.push(recipe);
+      favs.push(recipe);
     }
-  }
+  };
 }
 
 
