3aa7bac909532aff034b6fe715759dbbca0d4718
Sean Leas <phoenix_24leas.com>
2020-07-09 19:45:35 -0600

    Create first few tests for pantry class #19

--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -1,24 +1,27 @@
 /*eslint-disable*/
 const chai = require('chai');
 const expect = chai.expect;
-const User = require('../src/User')
+const User = require('../src/User');
 const userData = require('../data/users');
+const Pantry = require('../src/Pantry');
 
-// describe('Pantry', () => {
+describe('Pantry', () => {
 
-//   it('should be a function', () => {
-//     expect(Pantry).to.be.a('function');
-//   });
+  it('should be a function', () => {
+    expect(Pantry).to.be.a('function');
+  });
 
-//   it('should be an instance of Pantry', () => {
-//     const pantry = new Pantry();
+  it('should be an instance of Pantry', () => {
+    const pantry = new Pantry();
 
-//     expect(pantry).to.be.an.instanceof(Pantry);
-//   });
+    expect(pantry).to.be.an.instanceof(Pantry);
+  });
 
-//   it('should have a list of ingredients', () => {
-//     const pantry = new Pantry();
+  it('should have a list of ingredients', () => {
+    const pantry = new Pantry();
 
-//     expect(pantry).to.be.an.instanceof(Pantry);
-//   });
-// });
+    expect(pantry).to.be.an.instanceof(Pantry);
+  });
+
+  
+});
