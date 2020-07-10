5b9f55859557600c79048b8c9f3eadf2d44dec1d
Sean Leas <phoenix_24leas.com>
2020-07-10 17:39:59 -0600

    Create arg test for check pantry method #24

--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -4,6 +4,7 @@ const expect = chai.expect;
 const User = require('../src/User');
 const userData = require('../data/users');
 const Pantry = require('../src/Pantry');
+const Recipe = require('../src/Recipe');
 
 describe('Pantry', () => {
 
@@ -22,6 +23,23 @@ describe('Pantry', () => {
 
     expect(pantry).to.be.an.instanceof(Pantry);
   });
+
+  it('should have a checkPantry method', () => {
+    const pantry = new Pantry();
+
+    pantry.checkPantry()
+
+    expect(pantry.checkPantry).to.be.a('function');
+  });
+  it('should be able to accept a recipe as an argument', () => {
+    const recipe = new Recipe();
+    const pantry = new Pantry();
+    const result = pantry.checkPantry(recipe);
+
+    expect(result).to.equal(recipe);
+  });
+  
+
 
   
 });
