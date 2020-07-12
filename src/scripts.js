05f504e95b559036ee50b5cc891037d7c8059681
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-07-11 18:56:02 -0600

    Create function loadrecipes #32

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,24 @@
-console.log('Hello world');
+// const recipesData = require("../data/recipes");
+const recipesDisplay = document.querySelector('.recipes-display');
+// const currentUser = new User();
+
+window.addEventListener('load', loadRecipes);
+
+function loadRecipes() {
+  recipesDisplay.innerHTML = '';
+  recipeData.forEach(recipe => {
+    recipesDisplay.innerHTML += `
+      <div class='recipe-card'>
+        <img class='recipe-img' src=${recipe.image} alt='picture of ${recipe.name}'/>
+        <footer class='card-footer'>
+          <p>${recipe.name}</p>
+          <div class='card-buttons'>
+            <button><img src="" alt="">Fav</button>
+            <button><img src="" alt="">Menu</button>
+            <button><img src="" alt="">$$$</button>
+          </div>
+        </footer>
+      </div>
+    `;
+  });
+}
