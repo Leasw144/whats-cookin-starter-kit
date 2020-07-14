8a768a2699fd2b3f26fdecf8cb1ce494d68e7d0a
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-07-13 19:44:21 -0600

    Split toggle favorites into add and remove #30

--- src/User.js
+++ src/User.js
@@ -9,16 +9,31 @@ class User {
     this.menu = new Menu();
   }
 
-  toggleFavorite = (recipe) => {
-    const favs = this.favoriteRecipes;
-    if (favs.includes(recipe)) {
-      const i = favs.indexOf(recipe);
-      favs.splice(i, 1);
-    } else {
-      favs.push(recipe);
-    }
+  // toggleFavorite(recipe) {
+  //   const favs = this.favoriteRecipes;
+  //   const recipeExists = favs.some(fav => fav.id === recipe.id);
+  //   /*if (favs.length === 0) {
+  //     favs.push(recipe);
+  //   } else */if (recipeExists) {
+  //     const i = favs.indexOf(recipe);
+  //     favs.splice(i, 1);
+  //   } else {
+  //     favs.push(recipe);
+  //   }
+  // }
+
+  addFavorite(recipe) {
+    this.favoriteRecipes.push(recipe); // Need to add to tests
   }
 
+  removeFavorite(recipe) {
+    this.favoriteRecipes.forEach((item, index) => {
+      if (item.id === recipe.id) {
+        this.favoriteRecipes.splice(index, 1); // Need to add to tests
+      }
+    });
+  }
+  
   filterByType(collection, tag) {
     return collection.filter((recipe) => {
       return recipe.tags.includes(tag);
