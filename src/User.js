e7b82637c41aa4ff1739e98c33935e433d4a2571
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-07-13 22:11:13 -0600

    Delete togglefavorites method #30

--- src/User.js
+++ src/User.js
@@ -9,19 +9,6 @@ class User {
     this.menu = new Menu();
   }
 
-  // toggleFavorite(recipe) {
-  //   const favs = this.favoriteRecipes;
-  //   const recipeExists = favs.some(fav => fav.id === recipe.id);
-  //   /*if (favs.length === 0) {
-  //     favs.push(recipe);
-  //   } else */if (recipeExists) {
-  //     const i = favs.indexOf(recipe);
-  //     favs.splice(i, 1);
-  //   } else {
-  //     favs.push(recipe);
-  //   }
-  // }
-
   addFavorite(recipe) {
     this.favoriteRecipes.push(recipe); // Need to add to tests
   }
