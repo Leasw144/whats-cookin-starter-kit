531140b8d472cbefa9c7f495c225ee5792916fbe
Sean Leas <phoenix_24leas.com>
2020-07-15 17:13:43 -0600

    Remove subtraction code from check pantry class

--- src/Pantry.js
+++ src/Pantry.js
@@ -3,26 +3,20 @@ class Pantry {
     this.ingredients = pantry
   }
 
-
   checkPantry(menu) {
-    const test = [menu]
-    console.log(test)
-    return test.reduce((groceryList, recipe) => {
+    const fullRecipe = [menu]
+    return fullRecipe.reduce((groceryList, recipe) => {
       recipe.ingredients.forEach(ingredient => {
         this.ingredients.forEach(pantryItem => {
           if (pantryItem.ingredient === ingredient.id && pantryItem.amount < ingredient.quantity.amount) {
-            const amountNeeded = ingredient.quantity.amount - pantryItem.amount
-            ingredient.quantity.amount = amountNeeded
             groceryList.push(ingredient)
           }
-        })
-      })
+        });
+      });
       return groceryList
-    }, [])
+    }, []);
   }
 } 
-
-
 
 if (typeof module !== 'undefined') {
   module.exports = Pantry;
